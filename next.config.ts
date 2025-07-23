28aa820a9e1eb5efee10cf430ac069ee90c6946c
Sam <flightunderwings@gmail.com>
2025-07-23 22:39:23 +0000

    how do i do that offline?

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,13 @@
-import type {NextConfig} from 'next';
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
+const withPWA = require('@ducanh2912/next-pwa').default({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+});
+
+const nextConfig = {
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
@@ -20,4 +27,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = withPWA(nextConfig);
