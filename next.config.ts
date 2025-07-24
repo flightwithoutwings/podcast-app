9353c190c96651a7309e71e7f2c24cb8fefd92a4
Sam <flightunderwings@gmail.com>
2025-07-24 19:58:23 +0000

    i had git merge issues. look over code and make sure recent changes were

--- next.config.ts
+++ next.config.ts
@@ -9,8 +9,22 @@ const withPWA = require('@ducanh2912/next-pwa').default({
 
 const repoName = process.env.GITHUB_REPOSITORY ? process.env.GITHUB_REPOSITORY.split('/')[1] : '';
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = ''
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
+
 const nextConfig = {
   output: 'export',
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   typescript: {
     ignoreBuildErrors: true,
   },
