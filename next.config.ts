0b895a74e8022c06ded75503e93fa8144fb9d819
Sam <flightunderwings@gmail.com>
2025-07-24 19:50:31 +0000

    ui on github pages doesnt have css. also the import podcast button isnt

--- next.config.ts
+++ next.config.ts
@@ -7,8 +7,12 @@ const withPWA = require('@ducanh2912/next-pwa').default({
   disable: process.env.NODE_ENV === 'development',
 });
 
+const repoName = process.env.GITHUB_REPOSITORY ? process.env.GITHUB_REPOSITORY.split('/')[1] : '';
+
 const nextConfig = {
   output: 'export',
+  basePath: repoName ? `/${repoName}` : '',
+  assetPrefix: repoName ? `/${repoName}/` : '',
   typescript: {
     ignoreBuildErrors: true,
   },
